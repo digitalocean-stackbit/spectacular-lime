989cd491034a3703b55b55fa66261ad8ea57a0f6
digitalocean-stackbit <77975946+digitalocean-stackbit@users.noreply.github.com>
2021-01-29 23:53:00 +0200

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -7,6 +7,10 @@ set -v
 echo "stackbit-build.sh: start build"
 
 # build site
+mkdir $HOME/src
+cd $HOME/src
+git clone https://github.com/gohugoio/hugo.git
+cd hugo
 go install --tags extended
 hugo
 
