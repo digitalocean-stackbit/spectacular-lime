0219fa5b90ba11a84b291c9f2933c977075ef077
digitalocean-stackbit <77975946+digitalocean-stackbit@users.noreply.github.com>
2021-02-01 12:21:22 +0200

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -5,14 +5,27 @@ set -o pipefail
 set -v
 
 echo "stackbit-build.sh: start build"
+hugo_versions=(
+  "0.80.0"
+  "0.48"
+)
 
-# build site
-mkdir $HOME/src
-cd $HOME/src
-git clone https://github.com/gohugoio/hugo.git
-cd hugo
-go install --tags extended
-cd /workspace
+mkdir -p hugo-bin
+cd hugo-bin
+
+for version in ${hugo_versions[@]}; do
+    file_name="hugo_extended_${version}_Linux-64bit.tar.gz"
+    version_url="https://github.com/gohugoio/hugo/releases/download/v${version}/${file_name}"
+    target_dir="v${version}"
+    echo "downloading hugo version ${version} from ${version_url} into hugo-bin/${target_dir}"
+#    curl -OL $version_url
+    wget $version_url --quiet
+    mkdir -p $target_dir
+    tar -xf $file_name -C $target_dir
+    rm $file_name
+    echo "finished downloading hugo version ${version}"
+done
+
 hugo
 
 echo "stackbit-build.sh: finished build"
